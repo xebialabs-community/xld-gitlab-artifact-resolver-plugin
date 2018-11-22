87cec1b94efca9018f0d7e9995405ca7f91d31b8
Benoit Moussaud <bmoussaud@xebialabs.com>
2018-11-22 06:10:45 +0100

    artifact resolver ok ( no security managed)

---
+++ README.md
@@ -0,0 +1,34 @@
+# XL Deploy GitLab artifact resolver plugin v1.0.0
+
+[![Build Status][xld-gitlab-artifact-resolver-plugin-travis-image]][xld-gitlab-artifact-resolver-plugin-travis-url]
+[![License: MIT][xld-gitlab-artifact-resolver-plugin-license-image]][xld-gitlab-artifact-resolver-plugin-license-url]
+![Github All Releases][xld-gitlab-artifact-resolver-plugin-downloads-image]
+
+[xld-gitlab-artifact-resolver-plugin-travis-image]: https://travis-ci.org/xebialabs-community/xld-gitlab-artifact-resolver-plugin.svg?branch=master
+[xld-gitlab-artifact-resolver-plugin-travis-url]: https://travis-ci.org/xebialabs-community/xld-gitlab-artifact-resolver-plugin
+[xld-gitlab-artifact-resolver-plugin-license-image]: https://img.shields.io/badge/License-MIT-yellow.svg
+[xld-gitlab-artifact-resolver-plugin-license-url]: https://opensource.org/licenses/MIT
+[xld-gitlab-artifact-resolver-plugin-downloads-image]: https://img.shields.io/github/downloads/xebialabs-community/xld-gitlab-artifact-resolver-plugin/total.svg
+
+## Preface
+
+This document describes the functionality provided by the XL Deploy Artifact resolver plugin.
+
+See the [XL Deploy reference manual](https://docs.xebialabs.com/xl-deploy) for background information on XL Deploy and deployment automation concepts.  
+
+## Overview
+
+## Requirements
+
+Note:  XLD version should not be lower than lowest supported version.  See <https://support.xebialabs.com/hc/en-us/articles/115003299946-Supported-XebiaLabs-product-versions>.
+
+## Installation
+
+* Copy the latest JAR file from the [releases page](https://github.com/xebialabs-community/xld-gitlab-artifact-resolver-plugin/releases) into the `XL_DEPLOY|RELEASE_SERVER/plugins` directory.
+* Copy [java-gitlab-api-4.1.0.jar](http://central.maven.org/maven2/org/gitlab/java-gitlab-api/4.1.0/java-gitlab-api-4.1.0.jar) into the `XL_DEPLOY|RELEASE_SERVER/plugins` directory
+* Restart the XL Deploy|Release server.
+
+## Features/Usage/Types/Tasks
+
+## References
+
---
+++ src/main/java/com/xebialabs/community/xldeploy/GitLabArtifactResolver.java
@@ -0,0 +1,130 @@
+/**
+ * Copyright 2018 XEBIALABS
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.xebialabs.community.xldeploy;
+
+import com.xebialabs.deployit.engine.spi.artifact.resolution.ArtifactResolver;
+import com.xebialabs.deployit.engine.spi.artifact.resolution.ArtifactResolver.Resolver;
+import com.xebialabs.deployit.engine.spi.artifact.resolution.ResolvedArtifactFile;
+import com.xebialabs.deployit.plugin.api.udm.artifact.SourceArtifact;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.codec.binary.Base64;
+import org.gitlab.api.GitlabAPI;
+
+
+import org.gitlab.api.models.GitlabProject;
+import org.gitlab.api.models.GitlabRepositoryFile;
+
+
+
+@Resolver(protocols = {"gitlab"})
+public class GitLabArtifactResolver implements ArtifactResolver {
+
+    @Override
+    public ResolvedArtifactFile resolveLocation(SourceArtifact artifact) {
+
+
+        System.out.println("GitLabArtifactResolver:" + artifact.getId());
+
+        GitlabAPI gitlabAPI = GitlabAPI.connect("https://gitlab.com", "hUqgKSEqKrq5n1dXxSkN");
+
+        System.out.println(gitlabAPI);
+
+        try {
+            Map<String, String> decodeGitLabURI = decodeGitLabURI(artifact.getFileUri());
+            GitlabProject project = gitlabAPI.getProject(decodeGitLabURI.get("project"));
+            System.out.println("GitLabArtifactResolver:" + project.getName());
+
+            final GitlabRepositoryFile repositoryFile = gitlabAPI.getRepositoryFile(project, decodeGitLabURI.get("path"), decodeGitLabURI.get("ref"));
+            System.out.println("GitLabArtifactResolver repositoryFile = " + repositoryFile);
+
+            return new ResolvedArtifactFile() {
+                @Override
+                public String getFileName() {
+                    return repositoryFile.getFileName();
+                }
+
+                @Override
+                public InputStream openStream() throws IOException {
+                    Base64 decoder = new Base64();
+                    return new ByteArrayInputStream(decoder.decode(repositoryFile.getContent()));
+                }
+
+                @Override
+                public void close() throws IOException {
+                    //acmeCloudClient.cleanTempDirs();
+                }
+            };
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("resolveLocation(" + artifact + ") failed", e);
+
+        }
+
+    }
+
+    static Map<String, String> decodeGitLabURI(String uri) {
+        String[] split = uri.split(":");
+        if (split.length != 4)
+            throw new RuntimeException("invalid gitlab uri (" + uri + ") the format is gitlab:projectid:ref:file");
+
+        Map<String, String> decoded = new HashMap<String, String>();
+        decoded.put("scheme", split[0]);
+        decoded.put("project", split[1]);
+        decoded.put("ref", split[2]);
+        decoded.put("path", split[3]);
+        return decoded;
+    }
+
+    @Override
+    public boolean validateCorrectness(SourceArtifact artifact) {
+        try {
+            return new URI(artifact.getFileUri()).getScheme().equals("gitlab");
+        } catch (URISyntaxException e) {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        //curl --header "Private-Token: hUqgKSEqKrq5n1dXxSkN" https://gitlab.com/api/v4/projects/9539714
+        //curl --header "Private-Token: hUqgKSEqKrq5n1dXxSkN" https://gitlab.com/api/v4/projects/9539714/repository/files/namespace.yaml?ref=master
+        //curl --header "Private-Token: hUqgKSEqKrq5n1dXxSkN" https://gitlab.com/api/v4/projects/9539714/repository/files/namespace.yaml/raw?ref=master
+
+        String url = "gitlab:9539714:master:namespace.yaml";
+
+        System.out.println("split length= " + GitLabArtifactResolver.decodeGitLabURI(url));
+
+        GitlabAPI gitlabAPI = GitlabAPI.connect("https://gitlab.com", "hUqgKSEqKrq5n1dXxSkN");
+
+        System.out.println(gitlabAPI);
+
+        GitlabProject project = gitlabAPI.getProject("9539714");
+        System.out.println(project.getName());
+
+        GitlabRepositoryFile master = gitlabAPI.getRepositoryFile(project, "namespace.yaml", "master");
+        System.out.println("master = " + master);
+
+        Base64 decoder = new Base64();
+        byte[] decodedBytes = decoder.decode(master.getContent());
+        System.out.println(new String(decodedBytes) + "\n");
+
+
+    }
+}
